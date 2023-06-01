f4d8895fa1c18aaf30c9fcf3aabef31918015a92
kolawole olaniyi <kolaniyi3@gmail.com>
2023-06-01 16:44:02 +0100

    page component setup

---
+++ client/src/store/component/IntroductionPage/IntroductionPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./introductionPage.css";
+
+const IntroductionPage = () => {
+  return <div>Hello this is the Introduction Page...</div>;
+};
+
+export default IntroductionPage;
---
+++ client/src/store/component/RoomPage/RoomPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RoomPage = () => {
+  return <div>Hello this is the Room Page...</div>;
+};
+
+export default RoomPage;
